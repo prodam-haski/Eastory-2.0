0cf1b4c0cc1af32af9c8537670c3d73628129b9d
prodam-haski <danyamaryankov@mail.ru>
2020-04-28 10:51:15 +0300

    подготовлены методы под клиент-серверное взаимодействие вот здесь

---
+++ app/src/main/java/com/prodadimhaski/eastory2/serverUtils/Client.java
@@ -0,0 +1,99 @@
+package com.prodadimhaski.eastory2.serverUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Client {
+    private static String ADDRESS = "";
+    private static int PORT = 7700;
+
+    private boolean connected = false;
+    private Net net;
+
+    private String serverName;
+    private String userName;
+
+    private Thread connectingRun = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (connected){
+                Socket socket = null;
+                try {
+                    socket = new Socket(InetAddress.getByName(ADDRESS),PORT);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                InputStream inputStream = null;
+                try {
+                    inputStream = socket.getInputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    inputStream.read();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
+
+    public Client(String serverName, String userName) throws IOException, SocketException {
+        this.serverName = serverName;
+        this.userName = userName;
+        net = new Net(PORT);
+
+        connected = net.openConnection(ADDRESS);
+        if (!connected) {
+            System.out.println("Connection failed..");
+        }
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public void createSession(){
+        String request = "/cr/"+serverName;
+        net.send(request.getBytes());
+        String message = net.receive();
+        message = message.substring(0,message.lastIndexOf("%"));
+        System.out.println(message);
+    }
+
+
+    public void getSessionList(){
+        String connectionPacket = "/ls/";
+        net.send(connectionPacket.getBytes());
+        String message = net.receive();
+        message = message.substring(0,message.lastIndexOf("%"));
+        System.out.println(message);
+
+    }
+
+    public void joinToSession(){
+        String connectionPacket = "/ad/"+serverName+"%"+userName+"%";
+        net.send(connectionPacket.getBytes());
+        String message = net.receive();
+        message = message.substring(0,message.lastIndexOf("%"));
+        System.out.println(message);
+    }
+
+    public void sendQuestion(String questions){
+        String request = "/lt/"+serverName+"%"+questions;
+        net.send(request.getBytes());
+        String message = net.receive();
+        message = message.substring(0,message.lastIndexOf("%"));
+        System.out.println(message);
+    }
+
+    public void sendResult(String result){
+        String request = "/re/"+serverName+"%"+userName+result+"%";
+        net.send(request.getBytes());
+    }
+
+
+}
